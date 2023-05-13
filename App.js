1343d423280fc0fb8e16328f0396065233dcdad9
MikhailS89 <mihey2023@ya.ru>
2023-05-13 12:04:31 +0400

    fix-1 create to new project

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello to new project "My Notes"</Text>
       <StatusBar style="auto" />
     </View>
   );
