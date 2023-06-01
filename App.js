11d97934fab16e569329120d328752e0d8c77e2f
MikhailS89 <mihey2023@ya.ru>
2023-06-01 20:32:02 +0400

    add bottom navigation

--- App.js
+++ App.js
@@ -1,17 +1,9 @@
 import { StyleSheet, View } from 'react-native';
 import Router from './router/routers';
-// import Search from './components/Search';
-// import Footer from './components/Footer';
-// import Header from './components/Header';
-// import List from './components/List';
 
 export default function App() {
   return (
     <View style={styles.home}>
-      {/* <Search />
-      <Footer />
-      <List />
-      <Header /> */}
       <Router />
     </View>
   );
--- router/routers.js
+++ router/routers.js
@@ -1,40 +1,66 @@
 import * as React from 'react';
-import Main from '../components/pages/Main';
-import Contacts from '../components/pages/Contacts';
-import Favourites from '../components/pages/Favourites';
-import Settings from '../components/pages/Settings';
+import Main from '../components/pages/Main/Main';
+// import Contacts from '../components/pages/Contacts/Contacts';
+import Favourites from '../components/pages/Favourites/Favourites';
+import Settings from '../components/pages/Settings/Settings';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
+// const Tab = createBottomTabNavigator();
+// const Tab = createMaterialTopTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 export default function Navigate() {
 
+    // return (
+    //   <NavigationContainer>
+    //     <Stack.Navigator>
+    //         <Stack.Screen
+    //             name="Main"
+    //             component={Main}
+    //             options={{ title: 'Главная' }}
+    //         />
+    //         <Stack.Screen
+    //             name="Contacts"
+    //             component={Contacts}
+    //             options={{ title: 'Контакты' }}
+    //         />
+    //         <Stack.Screen
+    //             name="Favourites"
+    //             component={Favourites}
+    //             options={{ title: 'Избранное' }}
+    //         />
+    //         <Stack.Screen
+    //             name="Settings"
+    //             component={Settings}
+    //             options={{ title: 'Настройки' }}
+    //         />
+    //     </Stack.Navigator>
+    //   </NavigationContainer>
+    // );
+
     return (
-      <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen
-                name="Main"
-                component={Main}
-                options={{ title: 'Главная' }}
-            />
-            <Stack.Screen
-                name="Contacts"
-                component={Contacts}
-                options={{ title: 'Контакты' }}
-            />
-            <Stack.Screen
-                name="Favourites"
-                component={Favourites}
-                options={{ title: 'Избранное' }}
-            />
-            <Stack.Screen
-                name="Settings"
-                component={Settings}
-                options={{ title: 'Настройки' }}
-            />
-        </Stack.Navigator>
-      </NavigationContainer>
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen
+                    name="Main"
+                    component={Main}
+                    // options={{ header: false }}
+                />
+                <Tab.Screen
+                    name="Favourites"
+                    component={Favourites}
+                />
+                <Tab.Screen
+                    name="Settings"
+                    component={Settings}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
     );
   }
