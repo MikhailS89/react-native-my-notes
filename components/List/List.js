f0548d19593cb9f1ab3f14d4b2be37f470ef404b
MikhailS89 <mihey2023@ya.ru>
2023-06-13 11:10:42 +0400

    add style component List

--- components/List/List.js
+++ components/List/List.js
@@ -1,16 +1,45 @@
 import React from 'react';
-import { StyleSheet, View, Text, StatusBar } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 
 
 export default function List() {
   return (
-    <View>
-      <Text>List component</Text>
-      <StatusBar style="auto" />
+    <View style={styles.list}>
+      <Text style={styles.title}>Designing for emotions</Text>
+      <Text style={styles.text}>wireframes , ui ux design and emotion design Designing for users</Text>
+      <View style={styles.watch}>
+        <Entypo name="stopwatch" size={24} color="#312161" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  list: {
+    borderWidth: 3,
+    borderColor: 'white',
+    borderRadius: 25,
+    backgroundColor: '#ffc5f7',
+    padding: '5%'
+  },
+  title: {
+    fontSize: 24,
+    color: '#312161'
+  },
+  text: {
+    marginTop: 10,
+    fontSize: 16,
+    color: '#be77b4',
+    maxHeight: 100
+  },
+  watch: {
+    marginTop: 10,
+    padding: 5,
+    borderWidth: 3,
+    borderColor: 'white',
+    borderRadius: 25,
+    backgroundColor: '#ffc5f7',
+    width: 40
+  }
 });
--- components/pages/Main/Main.js
+++ components/pages/Main/Main.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { globalStyle } from '../../../styles/style';
 import Search from '../../Search/Search';
+import List from '../../List/List';
 
 export default function Main({ navigation }) {
   return (
     <View style={[styles.container, globalStyle.main]}>
       <Search />
-      <View>
-        <Text>Hello to new project "My Notes"</Text>
+      <View style={styles.container}>
+        <List />
       </View>
     </View>
   );
